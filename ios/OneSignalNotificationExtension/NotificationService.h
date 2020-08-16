51894b1721f66e7545c6577a64cba8025d56740b
hopaut <administration@hopaut.com>
2020-08-16 21:58:54 +0200

    OneSignal added to iOS

---
+++ ios/OneSignalNotificationExtension/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  OneSignalNotificationExtension
+//
+//  Created by MacBook Air on 16.08.20.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
