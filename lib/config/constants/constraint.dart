38a3244b830a6247b96ce32fb8a17d94d7d9a522
IvayloGatev <ivo993@abv.bg>
2021-10-06 22:14:09 +0200

    Rabese

--- lib/config/constants/constraint.dart
+++ lib/config/constants/constraint.dart
@@ -1,6 +1,9 @@
 class Constraint {
   // Posts ---------------------------------------------------------------------
+<<<<<<< HEAD
   static const int pictureMaxCount = 3;
+=======
+>>>>>>> e3d8000... Rabese
   static const int tagMaxLength = 20;
   static const int tagMaxCount = 5;
   static const int descriptionMaxLength = 200;
