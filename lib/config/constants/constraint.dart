6b14fb82223227e6b7de7611e7ccb79c613d869c
mironicagicu <mironicagicu@gmail.com>
2021-10-12 20:46:01 +0200

    Constrains

--- lib/config/constants/constraint.dart
+++ lib/config/constants/constraint.dart
@@ -1,12 +1,9 @@
 class Constraint {
   // Posts ---------------------------------------------------------------------
-<<<<<<< HEAD
   static const int pictureMaxCount = 3;
-=======
->>>>>>> e3d8000... Rabese
   static const int tagMaxLength = 20;
   static const int tagMaxCount = 5;
-  static const int descriptionMaxLength = 200;
+  static const int descriptionMaxLength = 3000;
   static const int descriptionMinLength = 10;
   static const int requirementsMaxLength = 500;
   static const int titleMaxLength = 30;
