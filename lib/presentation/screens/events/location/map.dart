95974cdd1ddf3cb10dac1b7a6a92477e4574af94
Hopaut UG <hopautug@Hopauts-MacBook-Air.local>
2021-11-01 21:40:43 +0100

    UI refactor

--- lib/presentation/screens/events/location/map.dart
+++ lib/presentation/screens/events/location/map.dart
@@ -4,6 +4,7 @@ import 'package:here_sdk/mapview.dart';
 import 'package:here_sdk/search.dart';
 import 'package:hopaut/config/constants.dart';
 import 'package:hopaut/config/routes/application.dart';
+import 'package:hopaut/controllers/providers/location_provider.dart';
 import 'package:hopaut/data/models/location.dart' as HopautLocation;
 import 'package:hopaut/controllers/providers/map_location_provider.dart';
 import 'package:hopaut/presentation/widgets/buttons/basic_button.dart';
@@ -61,11 +62,103 @@ class _SearchByMapState extends State<SearchByMap> {
         ),
         elevation: 0,
       ),
-      body: _body()
+      body: _body(locationSelectionProvider)
     );
   }
 
-  Widget _body() {
+  Widget _body(MapLocationProvider locationSelectionProvider) {
+    return Stack(
+      children: [
+        _map(locationSelectionProvider),
+        SafeArea(
+          child: Align(
+            alignment: Alignment.bottomCenter,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 32),
+              child: BasicButton(
+                onPressed: () async =>
+                  locationSelectionProvider.getReverseGeocodeResult(),
+              // TODO translation
+              label: 'Confirm'),
+            ),
+          ),
+        ),
+        _searchTextField(locationSelectionProvider),
+        SafeArea(
+          child: Visibility(
+            visible: locationSelectionProvider.searchResults.isNotEmpty,
+            child: Center(
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Container(
+                  padding: EdgeInsets.all(4),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    border: Border.all(),
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                  child: ListView.builder(
+                    itemCount: locationSelectionProvider.searchResults.length,
+                    shrinkWrap: true,
+                    itemBuilder: (context, index) {
+                      return ListTile(
+                        title: Text(
+                      locationSelectionProvider.searchResults[index].title),
+                      );
+                    }),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+  Widget _searchTextField(MapLocationProvider locationSelectionProvider){
+    return SafeArea(
+      child: Align(
+        alignment: Alignment.topCenter,
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Container(
+            decoration: BoxDecoration(
+              color: Colors.grey[200].withOpacity(0.7),
+              borderRadius: BorderRadius.circular(8),
+              border: Border.all(
+                  color: Colors.grey[300].withOpacity(0.7)),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.07),
+                  blurRadius: 3.0,
+                  spreadRadius: 2.0,
+                  offset: Offset(0, 1.5))
+              ]),
+            child: TypeAheadField(
+              textFieldConfiguration: TextFieldConfiguration(
+                keyboardType: TextInputType.text,
+                controller: locationSelectionProvider.searchBarController,
+                decoration: InputDecoration(
+                  contentPadding: EdgeInsets.all(12.0),
+                  hintText: 'Search',
+                  border: InputBorder.none,
+                )),
+              suggestionsCallback: (pattern) async =>
+              await locationSelectionProvider.getAutocompleteResult(pattern),
+              itemBuilder: (context, Place suggestion) => ListTile(
+                title: Text(suggestion.title),
+              ),
+              onSuggestionSelected: (Place suggestion) =>
+                  locationSelectionProvider.addToSearchResult(suggestion),
+              hideOnEmpty: true,
+              hideOnError: true,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _map(MapLocationProvider locationServiceProvider){
     return Stack(
       children: [
         Container(
@@ -92,112 +185,28 @@ class _SearchByMapState extends State<SearchByMap> {
                 onPressed: () => locationSelectionProvider.searchResultState != SearchResultState.IDLE
                     ? locationSelectionProvider.clearSearchResult()
                     : null,
-                ),
-                SizedBox(
-                  height: 5,
-                ),
-                // TODO translation
-                Text(' Tap to get location ',
-                  style: TextStyle(
-                    shadows: [
-                      Shadow(
-                        offset: Offset(3, 3),
-                        color: Colors.black.withOpacity(0.2),
-                        blurRadius: 5
-                      )
-                    ],
-                    backgroundColor: Colors.black.withOpacity(0.60),
-                    color: Colors.white,
-                  )
-                ),
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              // TODO translation
+              Text(' Tap to get location ',
+                style: TextStyle(
+                  shadows: [
+                    Shadow(
+                      offset: Offset(3, 3),
+                      color: Colors.black.withOpacity(0.2),
+                      blurRadius: 5
+                    )
+                  ],
+                  backgroundColor: Colors.black.withOpacity(0.60),
+                  color: Colors.white,
+                )
+              ),
             ],
           ),
         ),
-        SafeArea(
-          child: Align(
-            alignment: Alignment.bottomCenter,
-            child: Padding(
-              padding: const EdgeInsets.symmetric(vertical: 32),
-              child: BasicButton(
-                onPressed: () async =>
-                  locationSelectionProvider.getReverseGeocodeResult(),
-              // TODO translation
-              label: 'Confirm'),
-            ),
-          ),
-        ),
-        SafeArea(
-            child: Align(
-              alignment: Alignment.topCenter,
-              child: Consumer<MapLocationProvider>(
-                builder: (_, mapController, __) => Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Container(
-                    decoration: BoxDecoration(
-                        color: Colors.grey[200].withOpacity(0.7),
-                        borderRadius: BorderRadius.circular(8),
-                        border: Border.all(
-                            color: Colors.grey[300].withOpacity(0.7)),
-                        boxShadow: [
-                          BoxShadow(
-                              color: Colors.black.withOpacity(0.07),
-                              blurRadius: 3.0,
-                              spreadRadius: 2.0,
-                              offset: Offset(0, 1.5))
-                        ]),
-                    child: TypeAheadField(
-                      textFieldConfiguration: TextFieldConfiguration(
-                          keyboardType: TextInputType.text,
-                          controller: mapController.searchBarController,
-                          decoration: InputDecoration(
-                            contentPadding: EdgeInsets.all(12.0),
-                            hintText: 'Search',
-                            border: InputBorder.none,
-                          )),
-                      suggestionsCallback: (pattern) async =>
-                      await mapController.getAutocompleteResult(pattern),
-                      itemBuilder: (context, Place suggestion) => ListTile(
-                        title: Text(suggestion.title),
-                      ),
-                      onSuggestionSelected: (Place suggestion) =>
-                          mapController.addToSearchResult(suggestion),
-                      hideOnEmpty: true,
-                      hideOnError: true,
-                    ),
-                  ),
-                ),
-              ),
-            )),
-        SafeArea(
-          child: Consumer<MapLocationProvider>(
-            builder: (_, mapController, __) => Visibility(
-              visible: mapController.searchResults.isNotEmpty,
-              child: Center(
-                child: Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Container(
-                    padding: EdgeInsets.all(4),
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      border: Border.all(),
-                      borderRadius: BorderRadius.circular(5),
-                    ),
-                    child: ListView.builder(
-                        itemCount: mapController.searchResults.length,
-                        shrinkWrap: true,
-                        itemBuilder: (context, index) {
-                          return ListTile(
-                            title: Text(
-                                mapController.searchResults[index].title),
-                          );
-                        }),
-                  ),
-                ),
-              ),
-            ),
-          ),
-        )
-      ],
+      ]
     );
   }
 }
