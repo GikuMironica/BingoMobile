78aec124e56845080c8c51b311f5efb5bcfbf65c
Hopaut UG <hopautug@Hopauts-MacBook-Air.local>
2021-11-26 21:59:58 +0100

    Make event lists scrollable, use cupertino refresh indicator

--- lib/presentation/screens/events/event_list_view.dart
+++ lib/presentation/screens/events/event_list_view.dart
@@ -24,66 +24,72 @@ class EventsListView extends StatelessWidget {
               child: overlayBlurBackgroundCircularProgressIndicator(
                   context, 'Loading events'),
             )
-          : RefreshIndicator(
-              onRefresh: () async {
-                provider.eventsMap[listType] = EventList();
-                await provider.fetchEventList(listType);
-              },
-              child: SafeArea(
-                  top: false,
-                  bottom: false,
-                  child: CustomScrollView(primary: true, slivers: [
-                    SliverOverlapInjector(
-                      // This is the flip side of the SliverOverlapAbsorber
-                      // above.
-                      handle: NestedScrollView.sliverOverlapAbsorberHandleFor(
-                          context),
+          : SafeArea(
+              top: false,
+              bottom: false,
+              child: CustomScrollView(
+                physics: const BouncingScrollPhysics(parent: AlwaysScrollableScrollPhysics()),
+                slivers: [
+                    CupertinoSliverRefreshControl(
+                      refreshTriggerPullDistance: 30.0,
+                      refreshIndicatorExtent: 30.0,
+                      onRefresh: () async {
+                        provider.eventsMap[listType] = EventList();
+                        await provider.fetchEventList(listType);
+                      },
                     ),
-                    SliverPadding(
-                        padding: EdgeInsets.symmetric(vertical: 16.0),
-                        sliver: provider.eventsMap[listType].state ==
-                                    EventListState.idle &&
-                                provider.eventsMap[listType].events.isNotEmpty
-                            ? SliverFixedExtentList(
-                                itemExtent: 136,
-                                delegate: SliverChildBuilderDelegate(
-                                  (ctx, index) => InkWell(
-                                    onTap: () async {
-                                      int id = provider.eventsMap[listType]
-                                          .events[index].postId;
-                                      await Application.router.navigateTo(
-                                          context, '/event/$id',
-                                          transition: TransitionType.cupertino);
-                                    },
-                                    child: MiniPostCard(
-                                      miniPost: provider
-                                          .eventsMap[listType].events[index],
+                      SliverOverlapInjector(
+                        // This is the flip side of the SliverOverlapAbsorber
+                        // above.
+                        handle: NestedScrollView.sliverOverlapAbsorberHandleFor(
+                            context),
+                      ),
+                      SliverPadding(
+                          padding: EdgeInsets.symmetric(vertical: 16.0),
+                          sliver: provider.eventsMap[listType].state ==
+                                      EventListState.idle &&
+                                  provider.eventsMap[listType].events.isNotEmpty
+                              ? SliverFixedExtentList(
+                                  itemExtent: 136,
+                                  delegate: SliverChildBuilderDelegate(
+                                    (ctx, index) => InkWell(
+                                      onTap: () async {
+                                        int id = provider.eventsMap[listType]
+                                            .events[index].postId;
+                                        await Application.router.navigateTo(
+                                            context, '/event/$id',
+                                            transition: TransitionType.cupertino);
+                                      },
+                                      child: MiniPostCard(
+                                        miniPost: provider
+                                            .eventsMap[listType].events[index],
+                                      ),
                                     ),
-                                  ),
-                                  childCount: provider
-                                      .eventsMap[listType].events.length,
-                                ))
-                            : SliverFillRemaining(
-                                hasScrollBody: false,
-                                child: Center(
-                                    child: Column(
-                                        mainAxisAlignment:
-                                            MainAxisAlignment.center,
-                                        crossAxisAlignment:
-                                            CrossAxisAlignment.center,
-                                        children: [
-                                      SvgPicture.asset(
-                                        'assets/icons/svg/no_events_found.svg',
-                                        width: 100,
-                                        height: 100,
-                                      ),
-                                      SizedBox(
-                                        height: 10,
-                                      ),
-                                      Text(
-                                          'No events found') //TODO: translation
-                                    ])))),
-                  ])));
+                                    childCount: provider
+                                        .eventsMap[listType].events.length,
+                                  ))
+                              : SliverFillRemaining(
+                                  hasScrollBody: false,
+                                  child: Center(
+                                      child: Column(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.center,
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.center,
+                                          children: [
+                                        SvgPicture.asset(
+                                          'assets/icons/svg/no_events_found.svg',
+                                          width: 100,
+                                          height: 100,
+                                        ),
+                                        SizedBox(
+                                          height: 10,
+                                        ),
+                                        Text(
+                                            'No events found') //TODO: translation
+                                      ])))),
+                    ]),
+          );
     });
   }
 }
