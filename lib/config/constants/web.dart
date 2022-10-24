5b1f378c082d289bc18d359fd4aad9147ff5fd97
mironicagicu <mironicagicu@gmail.com>
2022-10-24 16:53:40 +0200

    Change img url to production

--- lib/config/constants/web.dart
+++ lib/config/constants/web.dart
@@ -1,5 +1,6 @@
 class WEB {
-  static const String BASE_URL = 'https://www.hop-out.com';
+  // static const String BASE_URL = 'https://www.hop-out.com';
+  static const String BASE_URL = 'https://www.hopaut.com';
   static const String IMAGES = '$BASE_URL/images';
   static const String PROFILE_PICTURES = '$IMAGES/profiles';
 
